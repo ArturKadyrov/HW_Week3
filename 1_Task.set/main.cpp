dbef09ef8a4b69d1b366a0ae0a09fa23866054b9
ArturKadyrov <kadyrov.ad@phystech.edu>
2022-02-20 21:17:13 +0300

    Добавил функцию , которая генерирует н случайных чисел.

--- 1_Task.set/main.cpp
+++ 1_Task.set/main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include "StopWatch.h"
 
-using namespace std;
+int * generate_ (int N)
+{
+    int * Array = new int [N];
+
+   srand( time( 0 ) );
+   for (int i =0 ; i<N; i++)
+    {
+        Array[i] = rand();
+    }
+
+    return Array;
+}
 
 int main()
 {
+    using namespace std;
+
+    int N =50;
+    int * Array = generate_(N);
+    for (int i=0;i<N;i++)
+    {
+        cout<<Array[i]<<endl;
+    }
+
+    delete [] Array;
 
 
     return 0;
